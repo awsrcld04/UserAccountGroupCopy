e042692c65f13b19c0afe0ae441444afae15e666
awsrcld04 <akil.woolfolk@gmail.com>
2025-04-12 15:14:17 -0400

    Removed funcLicenseActivation function block

--- UserAccountGroupCopy.cs
+++ UserAccountGroupCopy.cs
@@ -260,46 +260,6 @@ namespace UserAccountGroupCopy
 
         } // LicenseCheck
 
-        static bool funcLicenseActivation()
-        {
-            try
-            {
-                if (funcCheckForFile("TurboActivate.dll"))
-                {
-                    if (funcCheckForFile("TurboActivate.dat"))
-                    {
-                        TurboActivate.VersionGUID = "4935355894e0da3d4465e86.37472852";
-
-                        if (TurboActivate.IsActivated())
-                        {
-                            return true;
-                        }
-                        else
-                        {
-                            Console.WriteLine("A license for this product has not been activated.");
-                            return false;
-                        }
-                    }
-                    else
-                    {
-                        Console.WriteLine("TurboActivate.dat is required and could not be found.");
-                        return false;
-                    }
-                }
-                else
-                {
-                    Console.WriteLine("TurboActivate.dll is required and could not be found.");
-                    return false;
-                }
-            }
-            catch (Exception ex)
-            {
-                MethodBase mb1 = MethodBase.GetCurrentMethod();
-                funcGetFuncCatchCode(mb1.Name, ex);
-                return false;
-            }
-        }
-
         static void funcLogToEventLog(string strAppName, string strEventMsg, int intEventType)
         {
             string sLog;
